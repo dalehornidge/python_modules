de77ef223c07e33d74732fc2798fb1eeff9acedf
dale hornidge <dalehornidge@gmail.com>
2023-03-10 15:27:46 +0000

    third commit

--- week_02/codeclan_caraoke/tests/room_test.py
+++ week_02/codeclan_caraoke/tests/room_test.py
@@ -2,3 +2,16 @@ import unittest
 from src.room import Room
 
 class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.room = Room("Room 10")
+
+    
+    def test_room_name(self):
+        self.assertEqual("Room 10", self.room.name)
+
+
+
+    # room name
+    # max occupants
+    # room playlist
+    # current guests
